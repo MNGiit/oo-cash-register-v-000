a7d1a64351129876a64f534ac6f7e8d2ef82280a
Mohammed Noori <kmanoorib@gmail.com>
2018-06-11 19:50:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,6 +4,7 @@ class CashRegister
   def initialize(employee_discount=20)
     @total = 0
     @employee_discount = employee_discount
+    @items = []
   end
   
   def add_item(title, price, quantity=1)
