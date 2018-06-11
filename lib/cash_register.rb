32f848ccf57ac9346646229118fe678f400d87b4
Mohammed Noori <kmanoorib@gmail.com>
2018-06-11 19:46:49 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :total, :employee_discount
   
-  def initialize(employee_discount = 0.2)
+  def initialize(employee_discount = 20)
     @total = 0
     @employee_discount = employee_discount
   end
